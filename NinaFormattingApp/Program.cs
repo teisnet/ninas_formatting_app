eb71fc5b8673703c1f92120f4f5b45c2af4ecede
Teis Draiby <teis@teis.net>
2019-06-22 18:37:13 +0200

    Output test data file to console

--- NinaFormattingApp/Program.cs
+++ NinaFormattingApp/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.IO;
 
 namespace NinaFormattingApp
 {
 	class Program
 	{
+		static string sourceText;
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("\nNINAS FORMATTING APP");
+			Console.WriteLine("------------------------------------------------------------------------------------------------\n");
+
+			sourceText = File.ReadAllText("testdata.csv");
+
+			Console.Write(sourceText);
+
+			Console.WriteLine("\nPress any key to quit");
+			Console.ReadKey(false);
 		}
 	}
 }
