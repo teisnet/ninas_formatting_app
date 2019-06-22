e09b509210cd81926311f08935d97aa3282f1fd9
Teis Draiby <teis@teis.net>
2019-06-22 21:51:04 +0200

    Add regex formatting and save output

--- NinaFormattingApp/Program.cs
+++ NinaFormattingApp/Program.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace NinaFormattingApp
 {
 	class Program
 	{
+		static string inputFilename = "testdata.csv";
+		static string outputFilename = "testdata_output.csv";
+
 		static string sourceText;
 
 		static void Main(string[] args)
@@ -12,12 +16,36 @@ namespace NinaFormattingApp
 			Console.WriteLine("\nNINAS FORMATTING APP");
 			Console.WriteLine("------------------------------------------------------------------------------------------------\n");
 
-			sourceText = File.ReadAllText("testdata.csv");
+			sourceText = File.ReadAllText(inputFilename);
 
-			Console.Write(sourceText);
+			string resultText = RegexProcessor.Process(sourceText);
+			Console.Write(resultText);
+
+			File.WriteAllText(outputFilename, resultText);
 
 			Console.WriteLine("\nPress any key to quit");
 			Console.ReadKey(false);
+		}
+	}
+
+
+	public static class RegexProcessor
+	{
+		static Regex regex = new Regex(@"^""([^""]*)\s(\w+)"",\s""""(.*)$", RegexOptions.Multiline);
+
+		public static string Process(string sourceText)
+		{
+			string resultText;
+
+			resultText = regex.Replace(sourceText, @"""$1"", ""$2""$3");
+
+			resultText = Regex.Replace(resultText, @"^(""[^""]*"", )(""[^""]*"")", (Match match) => {
+				return Regex.Replace(match.ToString(), @"\b\w", (Match innerMatch) => innerMatch.ToString().ToUpper());
+			}, RegexOptions.Multiline);
+
+			// resultText = resultText.Replace("\"", "");
+
+			return resultText;
 		}
 	}
 }
