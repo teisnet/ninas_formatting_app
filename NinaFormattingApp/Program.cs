a573746f8330a226adbffbe3d4defc3adcbc1812
Teis Draiby <teis@teis.net>
2019-06-23 14:40:15 +0200

    Add comments

--- NinaFormattingApp/Program.cs
+++ NinaFormattingApp/Program.cs
@@ -5,6 +5,10 @@ using System.Text.RegularExpressions;
 
 namespace NinaFormattingApp
 {
+	// App for formatting Nina's csv-formatted email subscription list.
+	// Converts the fullname field to a firstname and a lastname field.
+	// See 'testdata_expected_output.csv' for examples.
+
 	class Program
 	{
 		// static string inputFilename = "testdata.csv";
@@ -36,22 +40,42 @@ namespace NinaFormattingApp
 
 	public static class RegexProcessor
 	{
+		// Both regexes do only match entries with a non-empty firstname field and an empty lastname field
+		// Entries that have both a firstname and a lastname and entries that only have a lastname are left untouched.
+		// Though, all entries are subject to character casing conversion.
+
+		// Regex 1: "Peter Birger Olsen", "" -> "Peter Birger", "Olsen"
+		// All words sre placed in 'firstname' ($1) except last word which is placed in 'lastname' ($2).
 		// @"^""([^""]*)\s([\w-_\.]+)"",\s""""(.*)$", @"""$1"", ""$2""$3"
+
+		// Regex 2: "Peter Birger Olsen", "" -> "Peter", "Birger Olsen"
+		// First word in 'firstname' ($1), remaining words in 'lastname' ($2).
 		static Regex regex = new Regex(@"^""([\w-_\.]+)(\s([^""]*))"",\s""""(.*)$", RegexOptions.Multiline);
 
 		public static string Process(string sourceText)
 		{
 			string resultText;
 
+			// 1) Fullname to firstname/lastname conversion
 			resultText = regex.Replace(sourceText, @"""$1"", ""$3""$4");
 
+			// 2) Character casing
+			// Match the firstname field ($1) and the lastname field ($2) for character casing
 			resultText = Regex.Replace(resultText, @"^(""[^""]*"", )(""[^""]*"")", (Match match) => {
+
 				string result = match.ToString();
+
+				// Uppercase first character
 				result = Regex.Replace(result, @"\b\w", (Match innerMatch) => innerMatch.ToString().ToUpper());
+
+				// Lowercase remaining characters
 				// result = Regex.Replace(result, @"\B\w", (Match innerMatch) => innerMatch.ToString().ToLower());
+
 				return result;
+
 			}, RegexOptions.Multiline);
 
+			// 3) Remove double quotes
 			// resultText = resultText.Replace("\"", "");
 
 			return resultText;
